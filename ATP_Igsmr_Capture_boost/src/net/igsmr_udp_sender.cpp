f5b7f93e6722966a7d2d49f0916972038da6902f
hexu <hexu_1985@sina.com>
2018-04-18 06:28:57 +0800

    update Makefile

--- ATP_Igsmr_Capture_boost/src/net/igsmr_udp_sender.cpp
+++ ATP_Igsmr_Capture_boost/src/net/igsmr_udp_sender.cpp
@@ -3,13 +3,13 @@
 IgsmrUdpSender::IgsmrUdpSender(const std::string &serv_ip, int serv_port):
     serv_ip_(serv_ip), serv_port_(serv_port)
 {
-    udp.Connect(serv_ip_.c_str(), serv_port_);
+    udp_.connect(serv_ip_.c_str(), serv_port_);
 }
 
 void IgsmrUdpSender::send(const CollectionData &data)
 {
     std::pair<void *, int> serialized_data;
     serialized_data = serializer_.serialize(data);
-    udp.send(serialized_data.first, serialized_data.second);
+    udp_.send(static_cast<char *>(serialized_data.first), serialized_data.second);
 }
 
